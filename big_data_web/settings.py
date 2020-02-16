7b44e55aa9d5d1cbce62e207e83ab7d8c255436c
soon <1572055146@qq.com>
2020-02-16 17:43:56 +0800

    fix file is to large situation

--- big_data_web/settings.py
+++ big_data_web/settings.py
@@ -21,9 +21,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'x)*$63&1pevnsq+$w&r(be#b)&zl-r82z*e12=h2+ni%a9hm8+'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = False
-DEBUG = True
+# SECURITY WARNING: don't run with debug turned on in production! 
+DEBUG = False
+#DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
--- main/csv_tool.py
+++ main/csv_tool.py
@@ -8,11 +8,11 @@ import chardet
 def csv_to_database(file_path):
     global df
     try:
-        with open(file_path, 'rb') as e:
+        with open(file_path, 'rb') as e:#modi
             ans = chardet.detect(e.read(10000))
             ans = ans.get('encoding')
         print("encoding:", ans, '\nend_detect')
-        enc = r'utf-8' if (ans == r"utf-8") else r'gbk'
+        enc = 'utf-8' if (ans == "utf-8") else 'gbk'
         reader = pd.read_csv(os.path.normpath(file_path), encoding=enc, sep=',', iterator=True, dtype={'id': str, })
         i = 0
         while True:
@@ -27,11 +27,11 @@ def csv_to_database(file_path):
                 print("Iteration is stopped.")
             # 循环结束退出
             break
-        print('读取完毕,写入数据库')
+        print('读取完毕,写入数据库hhhh')
         start3 = time.clock()
         query_list = []
         for i, r in df.iterrows():
-            # AppUser(**(r.to_dict())).save() #每次save都访问一次数据库,效率太低
+            #user_cur.save() #每次save都访问一次数据库,效率太低
             if not AppUser.objects.filter(id=r['id']).exists():
                 query_list.append(AppUser(**(r.to_dict())))
         AppUser.objects.bulk_create(query_list)
